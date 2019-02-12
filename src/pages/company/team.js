23fab7adb97f69024961a5861a3a9e83096fff73
Carol Enache <enache.carol@gmail.com>
2019-02-12 23:55:53 +0000

    create first pages

---
+++ src/pages/company/team.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const Team = () => (
+    <Layout>
+        <div>
+            <h1>Team</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum maxime quibusdam debitis obcaecati non, explicabo vero fuga alias maiores magni blanditiis, minima accusantium! Odio maxime delectus soluta sunt laudantium harum.</p>
+        </div>
+    </Layout>
+)
+
+export default Team;
