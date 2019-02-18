933d206f2b5eabdb50960dc94cc4c18abacae9af
Carol Enache <enache.carol@gmail.com>
2019-02-18 21:35:01 +0000

    Gatsby node stage 1

---
+++ src/components/postLayout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import layout from './layout'
+import Layout from './layout';
+
+export default class postLayout extends Component {
+  render() {
+    return (
+        <Layout>
+            <div>
+                <h1>Pos Layout</h1>
+            </div>
+        </Layout>
+    )
+  }
+}
